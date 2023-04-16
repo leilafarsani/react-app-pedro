2122fc409ffb07dd2e90c86391599a8307d55d0f
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-04-16 14:41:49 +0100

    Set Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const greeting = "Learn React with Pedro";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World <br />
+          {greeting}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
